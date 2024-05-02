848ea997eea5f8264dc99544184d9fbb39ed222d
chiragpdudhat <chiragpdudhat@gmail.com>
2024-05-02 18:38:42 +0530

    add about photo

--- src/componet/Hero.js
+++ src/componet/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <>
             <div className='container'>
-                <div className=' mt-5 align-items-center d-flex'>
+                <div className='align-items-center d-flex'>
                     <div className='text-white '>
                         <h1>
                             Hello, I'm Parth Nandaniya
@@ -13,8 +13,8 @@ function Hero() {
                             UI & UX Designer
                         </h1>
                         <p>
-                            i'm a Parth Nandaniya based UI/UX Designer. DEsigner Creating
-                            Awesome And Effective Visul Identities For Companies Of AllSize
+                            i'm a Parth Nandaniya based UI/UX Designer. Designer Creating
+                            Awesome And Effective Visul Identities For Companies Of All Size
                             Around The Globe
                         </p>
                         <div className="d-flex mt-4 gap-3">
@@ -44,8 +44,8 @@ function Hero() {
                             <img
                                 className="object-cover object-center rounded"
                                 alt="hero"
-                                src="https://img.freepik.com/premium-photo/silhouette-man-looking-sunset_781078-81.jpg?w=900"
-                                width={600}
+                                src="image/parth.jpg"
+                                width={750}
                             />
                         </div>
 
