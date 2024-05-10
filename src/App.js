271980f09344e25040c4c51f49c2bf2de14b38d7
parth nandaniya <parthnandaniya62@gmail.com>
2024-05-10 14:26:59 +0530

    add custom scrollbar test

--- src/App.js
+++ src/App.js
@@ -7,14 +7,38 @@ import Hero from './componet/Hero';
 import Myproject from './componet/Myproject';
 
 function App() {
+
+
   const [animationData, setAnimationData] = useState(null);
   const [isloading, setisloading] = useState(true);
   const [animationIsPlaying, setAnimationIsPlaying] = useState(true);
 
   useEffect(() => {
+    const updateReadProgress = () => {
+      const blogContent = document.querySelector(".blog-content");
+      const readProgress = document.getElementById("read-progress");
+      const windowHeight = window.innerHeight;
+      const scrollHeight = blogContent.scrollHeight;
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      const percentScrolled = (scrollTop / (scrollHeight - windowHeight)) * 100;
+      readProgress.style.width = percentScrolled + "%";
+    };
+
+    window.addEventListener("scroll", updateReadProgress);
+
+    return () => {
+      window.removeEventListener("scroll", updateReadProgress);
+    };
+  }, []);
+
+
+
+  useEffect(() => {
     const fetchAnimationData = async () => {
       try {
-        const response = await fetch('loading.json'); // Adjust the path if needed
+        const response = await fetch('loading.json');
         const animationData = await response.json();
         setAnimationData(animationData);
       } catch (error) {
@@ -30,7 +54,6 @@ function App() {
       setisloading(false)
     }, 1000);
   }, [])
-  // Customize Lottie options
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -40,32 +63,47 @@ function App() {
     }
   };
 
-  // Function to handle animation completion
   const onAnimationComplete = () => {
     setAnimationIsPlaying(false);
   };
 
   return (
-    <div className="app-container">
-      {isloading ? (
-        <div className="loading-animation-container">
-          <Lottie
-            options={defaultOptions}
-            height={400}
-            width={400}
-            className="loading-animation"
-            eventListeners={[{ eventName: 'complete', callback: onAnimationComplete }]}
-          />
-        </div>
-      ) : (
-        <div className="content">
-          <Header />
-          <Hero />
-          <Myproject />
-          <Footer />
-        </div>
-      )}
+    <><div className="blog-content">
+      <div className="app-container">
+        {isloading ? (
+          <div className="loading-animation-container">
+            <Lottie
+              options={defaultOptions}
+              height={400}
+              width={400}
+              className="loading-animation"
+              eventListeners={[{ eventName: 'complete', callback: onAnimationComplete }]}
+            />
+          </div>
+        ) : (
+          <div className="content">
+            <Header />
+            <Hero />
+            <Myproject />
+            <Footer />
+          </div>
+        )}
+      </div>
     </div>
+      <div
+        id="read-progress"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: 0,
+          height: "6px",
+          borderRadius:"0 0 10px 10px",
+          backgroundImage: "linear-gradient(to right, rgb(122, 153, 217), rgb(28, 58, 148))",
+          zIndex: 9999,
+          transition: "width 0.3s ease",
+        }}
+      ></div></>
   );
 }
 
--- src/componet/Footer.js
+++ src/componet/Footer.js
@@ -42,48 +42,48 @@ function Footer() {
     };
     return (
         <>
-            <div class="container">
-                <div className='mt-4 text-center'>
+            {/* <div className="container">
+                <div classNameName='mt-4 text-center'>
                     <h1>Contact Us</h1>
                 </div>
                 <form onSubmit={handleSubmit}>
-                    <div className="d-flex gap-5 justify-content-center form_nameemail">
-                        <div className="text-center w-25 ">
+                    <div classNameName="d-flex gap-5 justify-content-center form_nameemail">
+                        <div classNameName="text-center w-25 ">
                             <label htmlFor="name">Name</label>
                             <input
                                 type="text"
                                 id="name"
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
-                                className="change mt-3"
+                                classNameName="change mt-3"
                                 placeholder="Enter Your Name"
                                 required
                             />
                         </div>
-                        <div className="text-center w-25">
+                        <div classNameName="text-center w-25">
                             <label htmlFor="email">Email</label>
                             <input
                                 type="email"
                                 id="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                                className="change mt-3"
+                                classNameName="change mt-3"
                                 placeholder="Enter Your Email"
                                 required
                             />
                         </div>
                     </div>
 
-                    <div className="container text-center ">
-                        <div className="row justify-content-center">
-                            <div className="col-6">
-                                <div className="form-group mt-3 form_textarea">
+                    <div classNameName="container text-center ">
+                        <div classNameName="row justify-content-center">
+                            <div classNameName="col-6">
+                                <div classNameName="form-group mt-3 form_textarea">
                                     <label htmlFor="message">Message</label>
                                     <textarea
                                         id="message"
                                         value={message}
                                         onChange={(e) => setMessage(e.target.value)}
-                                        className="change mt-3"
+                                        classNameName="change mt-3"
                                         rows="5"
                                         placeholder="Enter your message"
                                         required
@@ -97,26 +97,53 @@ function Footer() {
                         <button type="submit" className="btn_2">Submit</button>
                     </div>
                 </form>
+            </div> */}
+
+            <div class="container">
+                <div class="mt-4 text-center">
+                    <h1>Contact Us</h1>
+                </div>
+                <form><div class="d-flex gap-5 justify-content-center form_nameemail">
+                    <div class="text-center w-25 ">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" class="change mt-3" placeholder="Enter Your Name" required="" value="" />
+                    </div>
+                    <div class="text-center w-25">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" class="change mt-3" placeholder="Enter Your Email" required="" value="" />
+                    </div>
+                </div>
+                    <div class="container text-center ">
+                        <div class="row justify-content-center">
+                            <div class="col-6">
+                                <div class="form-group mt-3 form_textarea">
+                                    <label for="message">Message</label>
+                                    <textarea id="message" class="change mt-3" rows="5" placeholder="Enter your message" required=""></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center mt-5">
+                        <button type="submit" class="btn_2">Submit</button>
+                    </div>
+                </form>
             </div>
 
-
             {/* Footer componet */}
-            <footer class="footer mt-5 h3 ">
-                <div class="container d-flex gap-3 justify-content-center footer_icon">
+            <footer className="footer mt-5 h3 ">
+                <div className="container d-flex gap-3 justify-content-center footer_icon">
 
-                    <a href="https://api.whatsapp.com/send?phone=6355304734" target='_blenk' class="social-icon"><IoLogoWhatsapp /></a>
-                    <a href="https://www.facebook.com/profile.php?id=100072656256788" target='_blenk' class="social-icon "><FaFacebook /></a>
-                    <a href="https://t.me/+916355304734" class="social-icon " target='_blenk'><FaTelegram /></a>
-                    <a href="https://www.linkedin.com/in/parth-nandaniya-73b684224/" class="social-icon" target='_blenk'><FaLinkedin /></a>
+                    <a href="https://api.whatsapp.com/send?phone=6355304734" target='_blenk' className="social-icon"><IoLogoWhatsapp /></a>
+                    <a href="https://www.facebook.com/profile.php?id=100072656256788" target='_blenk' className="social-icon "><FaFacebook /></a>
+                    <a href="https://t.me/+916355304734" className="social-icon " target='_blenk'><FaTelegram /></a>
+                    <a href="https://www.linkedin.com/in/parth-nandaniya-73b684224/" className="social-icon" target='_blenk'><FaLinkedin /></a>
 
                 </div>
             </footer>
             <div className='text-center mt-3 mb-3 '>
                 <a href="https://mail.google.com" className='footer_mail' target='_blank'>parthnandaniya62@gmail.com</a>
             </div>
-            <div className="text-center bg-dark p-3 footer_buttom">
-                © 2024 Parth All Rights Reserved , Inc.
-            </div>
+            <div class="text-center bg-dark p-3 footer_buttom">© 2024 Parth All Rights Reserved , Inc.</div>
 
         </>
     )
